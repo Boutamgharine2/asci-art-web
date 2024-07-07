fc5678777e20c8a000954c90e1b7a295ec4b7200
Boutamgharine2 <anouarboutamgh@gmail.com>
2024-07-07 12:02:39 +0100

    handler

--- funcs/handler.go
+++ funcs/handler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func Handler_rout(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" { // handel if url was not valide
 		http.Error(w, "page not found 404", http.StatusNotFound)
 		return
@@ -14,30 +14,34 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "methode not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	tmpl,err := template.ParseFiles("tmplt/index.html")
-	if err!= nil {
-		http.Error(w,"internal server error 500",http.StatusInternalServerError)
+	tmpl, err := template.ParseFiles("tmplt/index.html")
+	r.ParseForm()
+	if err != nil {
+		http.Error(w, "internal server error 500", http.StatusInternalServerError)
 	}
 
 	tmpl.Execute(w, nil)
 }
 
+
 func Handler_asci_art(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost { // handel if url was not valide
+	if r.Method != http.MethodPost { 
 		http.Error(w, "methode not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	tmpl,err := template.ParseFiles("tmplt/index.html")
-	if err!= nil {
-		http.Error(w,"internal server error 500",http.StatusInternalServerError)
+	tmpl, err := template.ParseFiles("tmplt/index.html")
+	if err != nil {
+		http.Error(w, "internal server error 500", http.StatusInternalServerError)
 	}
 	r.ParseForm()
 	banner := r.Form.Get("style")
 	input := r.Form.Get("user_input")
-	if banner=="" || input=="" {
-		http.Error(w,"Bad Request 400",http.StatusBadRequest)
+	if banner == "" || input == "" {
+		http.Error(w, "Bad Request 400", http.StatusBadRequest)
+		return
 	}
 	v := Printing(input, banner)
+
 
 	tmpl.Execute(w, v)
 }
