ea7e9db87a4ec81c3d4a2645aa373918bcd0b6bb
Boutamgharine2 <anouarboutamgharin@gmail.com>
2024-07-11 16:49:06 +0100

    rr

--- funcs/handler.go
+++ funcs/handler.go
@@ -6,6 +6,7 @@ import (
 )
 
 func Handler_rout(w http.ResponseWriter, r *http.Request) {
+	
 	if r.URL.Path != "/" { // handel if url was not valide
 		http.Error(w, "page not found :)", http.StatusNotFound)
 		return
@@ -28,14 +29,16 @@ func Handler_asci_art(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "methode not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
 	tmpl, err := template.ParseFiles("tmplt/index.html")
 	if err != nil {
 		http.Error(w, "internal server error 500", http.StatusInternalServerError)
 	}
 	r.ParseForm()
+
 	banner := r.Form.Get("style")
 	input := r.Form.Get("user_input")
-	if (banner == "" || input == "" ) || ( banner != "standard" &&  banner != "shadow" && banner != "thinkertoy" )  {
+	if (banner == "" || input == "") || (banner != "standard" && banner != "shadow" && banner != "thinkertoy") {
 		http.Error(w, "Bad Request 400", http.StatusBadRequest)
 		return
 	}
